ae3429dca203fbc2b9fae1619c1cb739e9092397
ankit15697 <hackerankit5@gmail.com>
2020-03-18 14:20:53 +0530

    Initial

---
+++ spring-mvc-jdbc/src/main/java/com/spring/model/Employee.java
@@ -0,0 +1,49 @@
+package com.spring.model;
+// This file will contain the
+public class Employee {
+    private int id;
+    private String name;
+    private String city;
+    private String age;
+    private String departmentName;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getDepartmentName() {
+        return departmentName;
+    }
+
+    public void setDepartmentName(String departmentName) {
+        this.departmentName = departmentName;
+    }
+}
